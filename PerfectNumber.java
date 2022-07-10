59581f4caf4f9e2722791b078722a6e7189c41f4
bharatpathak1998 <bharatpathak099@gmail.com>
2022-07-10 16:56:52 +0530

    initial commit

---
+++ PerfectNumber.java
@@ -0,0 +1,30 @@
+package logicalprograms;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+
+    public static void main(String[] args) {
+		
+	System.out.print("Enter the number: ");
+	Scanner sc = new Scanner(System.in);
+	    
+	int number = sc.nextInt();
+	int sum = 0;
+	int i = 1;
+	    
+	while (i <= number / 2) {
+	if (number % i == 0) {
+	    sum = sum + i;
+	}
+	    i++;
+	}
+	if (sum == number) {
+	    System.out.println(number + " is a perfect number.");
+	}
+	else
+	{
+            System.out.println(number + " is not a perfect number.");
+        }
+    }
+}
