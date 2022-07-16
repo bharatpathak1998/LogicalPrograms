be5e8869cb2ae71d3817deefb06f0c1ca9ef27b4
Bharat Pathak <107098833+bharatpathak1998@users.noreply.github.com>
2022-07-16 23:18:53 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -8,11 +8,11 @@ public class FibonacciSeries {
 
 	Scanner sc = new Scanner(System.in);
         System.out.println("Enter a count: ");
-        int count = sc.nextInt();
+        int a = sc.nextInt();
 
         int num1 = 0, num2 = 1, num3, i;
         System.out.print(num1 + " " + num2);
-        for (i = 2; i < count; i++)
+        for (i = 2; i < a; i++)
         {
             num3 = num1 + num2;
             System.out.print(" " + num3);
